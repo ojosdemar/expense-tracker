4a6cde42e1e86f5521e4c7c063be4a899f891d7a
ojosdemar <ojosdemar1986@gmail.com>
2026-02-22 17:29:16 +0300

    Create ExpenseDatabase.kt

---
+++ app/src/main/java/com/example/expensetracker/data/local/database/ExpenseDatabase.kt
@@ -0,0 +1,29 @@
+package com.example.expensetracker.data.local.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.expensetracker.data.local.entity.ExpenseEntity
+
+@Database(entities = [ExpenseEntity::class], version = 1, exportSchema = false)
+abstract class ExpenseDatabase : RoomDatabase() {
+    abstract fun expenseDao(): ExpenseDao
+    
+    companion object {
+        @Volatile
+        private var INSTANCE: ExpenseDatabase? = null
+        
+        fun getDatabase(context: Context): ExpenseDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ExpenseDatabase::class.java,
+                    "expense_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
