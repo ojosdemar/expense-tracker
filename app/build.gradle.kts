2bed726f5673d6f45dacebdf4a83b1b4cb92f543
ojosdemar <ojosdemar1986@gmail.com>
2026-02-23 12:26:39 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
-    // НЕТ Navigation - используем FragmentManager напрямую
     
     // Room
     implementation("androidx.room:room-runtime:2.6.1")
