c3830941cc716328858b7e98ac1889e88763f60b
ojosdemar <ojosdemar1986@gmail.com>
2026-02-24 22:44:25 +0300

    Update AppModule.kt

--- app/src/main/java/com/example/expensetracker/di/AppModule.kt
+++ app/src/main/java/com/example/expensetracker/di/AppModule.kt
@@ -2,7 +2,9 @@ package com.example.expensetracker.di
 
 import android.content.Context
 import com.example.expensetracker.data.local.database.ExpenseDatabase
+import com.example.expensetracker.data.repository.CategoryRepositoryImpl
 import com.example.expensetracker.data.repository.ExpenseRepositoryImpl
+import com.example.expensetracker.domain.repository.CategoryRepository
 import com.example.expensetracker.domain.repository.ExpenseRepository
 import dagger.Module
 import dagger.Provides
@@ -14,18 +16,26 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-    
+
     @Provides
     @Singleton
     fun provideDatabase(@ApplicationContext context: Context): ExpenseDatabase {
         return ExpenseDatabase.getDatabase(context)
     }
-    
+
     @Provides
     @Singleton
     fun provideExpenseDao(database: ExpenseDatabase) = database.expenseDao()
-    
+
+    @Provides
+    @Singleton
+    fun provideCategoryDao(database: ExpenseDatabase) = database.categoryDao()
+
     @Provides
     @Singleton
     fun provideExpenseRepository(impl: ExpenseRepositoryImpl): ExpenseRepository = impl
+
+    @Provides
+    @Singleton
+    fun provideCategoryRepository(impl: CategoryRepositoryImpl): CategoryRepository = impl
 }
